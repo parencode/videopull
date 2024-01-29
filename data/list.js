0584e9d4835e981f3380ad73803b813e2454c22c
parencode <51156091+parencode@users.noreply.github.com>
2024-01-29 11:08:43 +0100

    Create list.js

---
+++ data/list.js
@@ -0,0 +1,19 @@
+let videosJSON = {
+  "videos": [
+       {
+      "titulo": "Cómo recortar tus vídeos con el editor de vídeos de YouTube Studio",
+      "artista": "YouTube Creators",
+      "enlace": "https://www.youtube.com/watch?v=2k_vGifQv78"
+    },
+    {
+      "titulo": "Cómo subir pódcasts centrados en el audio a YouTube con feeds RSS",
+      "artista": "YouTube Creators",
+      "enlace": "https://www.youtube.com/watch?v=zrb327agiDE"
+    },
+    {
+      "titulo": "Colab, novedades para Miembros del canal, pausar comentarios y más | Resumen mensual para creadores",
+      "artista": "YouTube Creators",
+      "enlace": "https://www.youtube.com/watch?v=6R1smCRniVA"
+    }
+  ]
+}
